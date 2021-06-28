99a9599fb7b20251f43005c7247ed90bda11e1e0
RHRiday <iiucrifat@gmail.com>
2021-06-28 13:42:37 +0600

    remote

--- connection/connect.php
+++ connection/connect.php
@@ -1,7 +1,25 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "register");
+// require __DIR__.'/../vendor/autoload.php';
+// $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__."/../");
+// $dotenv->load();
 
+//Get Heroku Remote connection information
+$remotedb_url = parse_url(getenv("REMOTE_DB"));
+
+$remotedb_server = $remotedb_url["host"];
+$remotedb_username = $remotedb_url["user"];
+$remotedb_password = $remotedb_url["pass"];
+$remotedb_db = substr($remotedb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($remotedb_server, $remotedb_username, $remotedb_password, $remotedb_db);  //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
 session_start();
 
 ?>
